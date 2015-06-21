f422179769a813af827ebcdd6f09287c14148aae
DELL <galby2992@gmail.com>
2015-06-21 20:02:36 +0300

    try location

--- app/src/main/java/com/example/eliavmenachi/simplelist/CreateCompanyActivity.java
+++ app/src/main/java/com/example/eliavmenachi/simplelist/CreateCompanyActivity.java
@@ -3,10 +3,17 @@ package com.example.eliavmenachi.simplelist;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Criteria;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import android.view.View;
 import android.widget.Button;
@@ -20,11 +27,22 @@ import com.example.eliavmenachi.simplelist.model.Company;
 import com.example.eliavmenachi.simplelist.model.Model;
 import com.example.eliavmenachi.simplelist.model.Post;
 import com.google.android.gms.common.api.GoogleApiClient;
-
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.maps.model.LatLng;
 
 public class CreateCompanyActivity extends Activity {
 
-    GoogleApiClient mGoogleApiClient;
+    protected GoogleApiClient mGoogleApiClient;
+    protected LocationRequest mLocationRequest;
+    protected LocationSettingsRequest mLocationSettingsRequest;
+    protected static final int REQUEST_CHECK_SETTINGS = 0x1;
+    // The minimum distance to change Updates in meters
+    private static final long MIN_UPDATE_DISTANCE = 10;
+    // The minimum time between updates in milliseconds
+    private static final long MIN_UPDATE_TIME = 1000 * 60;
+
     Location mLastLocation;
 
     @Override
@@ -32,10 +50,7 @@ public class CreateCompanyActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_company);
 
-        this.buildGoogleApiClient();
-
-        mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                mGoogleApiClient);
+        LatLng currLoc = this.getLocation();
         //GeoPoint goe = this.convert(mLastLocation.getLatitude(), mLastLocation.getLongitude());
 
 
@@ -68,6 +83,54 @@ public class CreateCompanyActivity extends Activity {
             }
         });
     }
+
+    public LatLng getLocation()
+    {
+        // Get the location manager
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        boolean isNetworkEnable = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        boolean isGpsEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+        String locationProvider = LocationManager.NETWORK_PROVIDER;
+        Location location1 = locationManager.getLastKnownLocation(locationProvider);
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+        Double lat,lon;
+        try {
+            lat = location.getLatitude ();
+            lon = location.getLongitude ();
+            return new LatLng(lat, lon);
+        }
+        catch (NullPointerException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+   /* @Override
+    public void onResult(LocationSettingsResult locationSettingsResult) {
+        final Status status = locationSettingsResult.getStatus();
+        switch (status.getStatusCode()) {
+            case LocationSettingsStatusCodes.SUCCESS:
+                // NO need to show the dialog;
+                break;
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                //  Location settings are not satisfied. Show the user a dialog
+                try {
+                    // Show the dialog by calling startResolutionForResult(), and check the result
+                    // in onActivityResult().
+                    status.startResolutionForResult((Activity)getApplicationContext(), REQUEST_CHECK_SETTINGS);
+                } catch (IntentSender.SendIntentException e) {
+                    //unable to execute request
+                }
+                break;
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                // Location settings are inadequate, and cannot be fixed here. Dialog not created
+                break;
+        }
+    }*/
 
   /* public static GeoPoint convert(double latitude, double longitude)
     {
